5f558dd5b0fbcff0a28a8060ccfd933fdcfc55fb
Himanshu Kapoor <himanshukapoor898@gmail.com>
2023-12-02 01:39:36 +0530

    navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,11 +5,13 @@ import About from './pages/About'
 import Profile from './pages/Profile'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
+import Header from './components/Header'
 export default function App() {
   return (
     <Router>
+      <Header />
         <Routes>
-          <Route path = '/' element = {<Homee />}/>
+          <Route path = '/' element = {<Home />}/>
           <Route path = '/sign-in' element = {<SignIn />}/>
           <Route path = '/sign-up' element = {<SignUp />}/>
           <Route path = '/about' element = {<About />}/>
